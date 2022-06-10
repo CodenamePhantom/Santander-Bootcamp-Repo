38bc04eac6fbe6c234bafa3fa0d4b8e3e271ed9c
CodenamePhantom <vitor.augusto064@gmail.com>
2022-06-10 16:47:29 -0300

    Aula javascript - contador

---
+++ aula-contador/README.md
@@ -0,0 +1,7 @@
+# AULA CONTADOR JS
+
+####Santander bootcamp
+
+contador HTML-CSS-JavaScript é bem fácil então eu fiz a minha versão sem
+seguir a aula.
+
---
+++ aula-contador/scripts/contador.js
@@ -0,0 +1,31 @@
+contadorStorage = localStorage
+
+const objetoContagem = document.getElementById("contagem-atual")
+const incrementarContagem = document.getElementById("incrementar")
+const decrementarContagem = document.getElementById("decrementar")
+const limparStorage = document.getElementById("limpar-storage")
+
+if (contadorStorage.length === 0) {
+  objetoContagem.innerHTML = 0
+} else {
+  objetoContagem.innerHTML = contadorStorage.value
+}
+
+incrementarContagem.addEventListener("click", () => {
+  var valorContagem = parseInt(objetoContagem.innerText)
+  objetoContagem.innerHTML = valorContagem + 1
+  contadorStorage.setItem("value", objetoContagem.innerText)
+})
+
+decrementarContagem.addEventListener("click", () => {
+  var valorContagem = parseInt(objetoContagem.innerText)
+  if (valorContagem > 0) {
+    objetoContagem.innerHTML = valorContagem - 1
+    contadorStorage.setItem("value", objetoContagem.innerText)
+  }
+})
+
+limparStorage.addEventListener("click", () => {
+  contadorStorage.clear()
+  document.location.reload(true)
+})
